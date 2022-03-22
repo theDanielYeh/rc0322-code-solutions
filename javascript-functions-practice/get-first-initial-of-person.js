cdab0dcb8aa9a02fc7f7a5cb2ac1f86173080365
Daniel Yeh <dyeh1989@gmail.com>
2022-03-22 23:10:36 +0000

    Completed javascript-functions-practice

---
+++ javascript-functions-practice/get-first-initial-of-person.js
@@ -0,0 +1,5 @@
+/* exported getFirstInitialOfPerson */
+function getFirstInitialOfPerson(person) {
+  var ans = person.firstName[0];
+  return ans;
+}
---
+++ javascript-functions-practice/get-right-triangle-area.js
@@ -0,0 +1,5 @@
+/* exported getRightTriangleArea */
+function getRightTriangleArea(base, height) {
+  var area = 1 / 2 * base * height;
+  return area;
+}
