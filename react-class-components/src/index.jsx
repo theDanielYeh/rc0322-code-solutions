0e16934c9988f0e58d665f470bccdc831621154c
Daniel Yeh <dyeh1989@gmail.com>
2022-05-19 00:25:46 +0000

    Completed react-class-components

---
+++ react-class-components/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// function CustomButton(props) {
+//   return (
+//     <button>{props.text}</button>
+//   );
+// }
+
+class CustomButton extends React.Component {
+  render() {
+    return <button>{this.props.text}</button>;
+  }
+}
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(element);
