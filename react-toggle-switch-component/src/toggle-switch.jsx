d18c216fa5e5f9c8a00a8f6423d4f2c5725109c1
Daniel Yeh <dyeh1989@gmail.com>
2022-05-20 18:23:38 +0000

    Deleted comment-outs

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -35,21 +35,3 @@ export default class ToggleSwitch extends React.Component {
     }
   }
 }
-
-// const ToggleSwitch = ({ label }) => {
-//   return (
-//     <div className="container">
-//       {label}{' '}
-//       <div className="toggle-switch">
-//         <input type="checkbox" className="checkbox"
-//           name={label} id={label} />
-//         <label className="label" htmlFor={label}>
-//           <span className="inner" />
-//           <span className="switch" />
-//         </label>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ToggleSwitch;
