dd3db48d70d7565378f0cfa452f0b1ec9fa87af2
Daniel Yeh <dyeh1989@gmail.com>
2022-06-23 21:15:54 +0000

    codewars: reversed strings

---
+++ algorithm/main.js
@@ -0,0 +1,9 @@
+// < !--codewars: Reversed Strings 8kyu-- >
+//   Complete the solution so that it reverses the string passed into it.
+
+// 'world' => 'dlrow'
+// 'word' => 'drow'
+
+function solution(str) {
+  return str.split('').reverse().join('');
+}
