e5bf85abe767f52d4aab832023166788d7c8b208
Daniel Yeh <dyeh1989@gmail.com>
2022-07-11 19:47:14 +0000

    codewars-if-you-cant-sleep

--- algorithm/codewars-testing-1-2-3.js
+++ algorithm/codewars-testing-1-2-3.js
@@ -1,13 +1,13 @@
-Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
 
-Write a function which takes a list of strings and returns each line prepended by the correct number.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
 
-The numbering starts at 1. The format is n: string.Notice the colon and space in between.
+// The numbering starts at 1. The format is n: string.Notice the colon and space in between.
 
-  Examples: (Input-- > Output)
+//   Examples: (Input-- > Output)
 
-  []-- > []
-  ["a", "b", "c"]-- > ["1: a", "2: b", "3: c"]
+//   []-- > []
+//   ["a", "b", "c"]-- > ["1: a", "2: b", "3: c"]
 
 var number = function (array) {
   for (let i = 0; i < array.length; i++) {
